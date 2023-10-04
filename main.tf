b58eb5af417d8cf2ce84ef17436abf72148735e7
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-10-05 01:00:41 +0530

    updated iam user module

--- main.tf
+++ main.tf
@@ -82,13 +82,23 @@ resource "aws_iam_user_group_membership" "ses_user" {
 
 module "ses_user" {
   source     = "registry.terraform.io/SevenPicoForks/iam-system-user/aws"
-  version    = "2.0.0"
+  version    = "2.0.1"
+  context    = module.context.self
   enabled    = module.context.enabled && local.create_user_enabled
   attributes = ["ses", "user"]
 
-  iam_access_key_max_age = var.iam_access_key_max_age
-  pgp_key                = var.pgp_key
-  context                = module.context.self
+  iam_access_key_max_age        = var.iam_access_key_max_age
+  create_iam_access_key         = var.create_iam_access_key
+  force_destroy                 = var.force_destroy
+  inline_policies               = var.inline_policies
+  inline_policies_map           = var.inline_policies_map
+  permissions_boundary          = var.permissions_boundary
+  path                          = var.path
+  policy_arns                   = var.policy_arns
+  policy_arns_map               = var.policy_arns_map
+  ssm_enabled                   = var.ssm_enabled
+  ssm_ignore_value_changes      = var.ssm_ignore_value_changes
+  ssm_ses_smtp_password_enabled = var.ssm_ses_smtp_password_enabled
 }
 
 
@@ -99,47 +109,4 @@ resource "aws_iam_user_policy" "sending_emails" {
   name   = module.context.id
   policy = join("", data.aws_iam_policy_document.ses_policy.*.json)
   user   = module.ses_user.user_name
-}
-
-# ------------------------------------------------------------------------------
-# KMS Key
-# ------------------------------------------------------------------------------
-#module "kms_key" {
-#  source  = "SevenPicoForks/kms-key/aws"
-#  version = "2.0.0"
-#  context = module.context.self
-#  enabled = module.context.enabled && local.create_user_enabled
-#
-#  customer_master_key_spec = "SYMMETRIC_DEFAULT"
-#  deletion_window_in_days  = var.kms_key_deletion_window_in_days
-#  description              = "KMS key for CI/CD ${module.context.id}"
-#  enable_key_rotation      = var.kms_key_enable_key_rotation
-#  key_usage                = "ENCRYPT_DECRYPT"
-#  multi_region             = false
-#}
-
-
-# ------------------------------------------------------------------------------
-# SSM Parameter Store Users Secrets Credential
-# ------------------------------------------------------------------------------
-module "store_write" {
-  source  = "cloudposse/ssm-parameter-store/aws"
-  version = "0.9.1"
-
-  count = module.context.enabled && local.create_user_enabled ? 1 : 0
-
-  ignore_value_changes = var.ssm_ignore_value_changes
-#  kms_arn              = module.kms_key.key_arn
-  parameter_write = [
-    {
-      name        = "/system_user/${module.ses_user.user_name}/ses_smtp_password"
-      value       = try(join("", module.ses_user.ses_smtp_password_v4), "unset")
-      type        = "SecureString"
-      overwrite   = true
-      description = "The SES_SMTP_PASSWORDv4 for the ${module.ses_user.user_name} user."
-    },
-  ]
-
-  context = module.context.legacy
-
 }
